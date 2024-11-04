c493c402130d1217910e09e8c49822ccfc7122e4
iriadda <iriadda@gmail.com>
2024-11-04 11:05:55 +0200

    HW 1 vars types cats

---
+++ HW_1_vars_types_cats/#6Qb97gsv/main.js
@@ -0,0 +1,5 @@
+let firstName = 'Ірина'
+let middleName = 'Тарасівна'
+let lastName = 'Середюк'
+let person = firstName + ' ' + middleName + ' ' + lastName
+console.log(person)
---
+++ HW_1_vars_types_cats/#ruUtWDUI/main.js
@@ -0,0 +1,4 @@
+let userName = prompt("Enter your name");
+let userMiddleName = prompt("Enter your middle name");
+let age = prompt("Enter your age number");
+console.log(userName + " " + userMiddleName + " " + age);
