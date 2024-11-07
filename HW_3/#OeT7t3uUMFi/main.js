bef4d329f1a07434933876f7ee00ef8879a79ddd
iriadda <iriadda@gmail.com>
2024-11-07 20:49:28 +0200

    HW 3 loops

---
+++ HW_3/#OeT7t3uUMFi/main.js
@@ -0,0 +1,5 @@
+let i = 0;
+while (i < 20) {
+    document.write('<div><h1>Hello world '+ i +' </h1></div>');
+    i++
+}
---
+++ HW_3/#TYj7ncx/main.js
@@ -0,0 +1,4 @@
+
+for (let i = 1; i <= 10; i++) {
+    document.write('<div>number of block '+ i +' </div>');
+}
